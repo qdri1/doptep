a3656007e525676e1163b0636990d210cb4a07c2
k.alimtayev <k.alimtayev@tele2.kz>
2026-03-03 09:45:09 +0500

    1.4.3

--- doptep/services/RevenueCatManager.swift
+++ doptep/services/RevenueCatManager.swift
@@ -11,7 +11,7 @@ final class RevenueCatManager: NSObject {
     static let shared = RevenueCatManager()
 
     // Replace with your actual API key from the RevenueCat dashboard
-    private let apiKey = "appl_XfdhOgttCOBDdtEfJunkgVHnOWD"
+    private let apiKey = "test_qCFhGPJIpQlLUhTPBpjrNYNOTmF"
 
     // Replace with your entitlement identifier from the RevenueCat dashboard
     private let entitlementId = "dop_tep_pro"
