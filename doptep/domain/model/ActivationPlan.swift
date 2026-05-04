47608578c5d0692f236be26d25fff55655acf5b2
k.alimtayev <k.alimtayev@tele2.kz>
2026-05-04 19:47:14 +0500

    one day access

--- doptep/domain/model/ActivationPlan.swift
+++ doptep/domain/model/ActivationPlan.swift
@@ -9,23 +9,9 @@ enum ActivationPlan: String, CaseIterable {
     case monthly = "subscription_monthly"
     case yearly = "annual_subscription"
     case unlimited = "lifetime_dop_tep"
+    case oneday = "one_day_dop_tep"
 
     var productId: String {
         return rawValue
-    }
-
-    var displayName: String {
-        switch self {
-        case .monthly: return NSLocalizedString("activation_plan_1", comment: "")
-        case .yearly: return NSLocalizedString("activation_plan_2", comment: "")
-        case .unlimited: return NSLocalizedString("activation_plan_3", comment: "")
-        }
-    }
-
-    var description: String {
-        switch self {
-        case .monthly, .yearly: return NSLocalizedString("activation_plan_desc_subscribe", comment: "")
-        case .unlimited: return NSLocalizedString("activation_plan_desc_lifetime", comment: "")
-        }
     }
 }
